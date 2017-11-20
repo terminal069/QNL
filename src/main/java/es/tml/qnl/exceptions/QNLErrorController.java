d341c18701f5dd13c8dc8d2f5039737fd8830b92
Javier Cerrato <jcerrato@paradigmadigital.com>
2017-11-20 10:06:48 +0100

    Error controller

--- src/main/java/es/tml/qnl/exceptions/QNLErrorController.java
+++ src/main/java/es/tml/qnl/exceptions/QNLErrorController.java
@@ -3,9 +3,11 @@ package es.tml.qnl.exceptions;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class QNLErrorController {
@@ -19,6 +21,18 @@ public class QNLErrorController {
 		
 		QNLGenericError genericError = new QNLGenericError();
 		genericError.setCode(e.getStatus().getReasonPhrase());
+		genericError.setErrorMessage(e.getMessage());
+		
+		return genericError;
+	}
+	
+	@ExceptionHandler(value = Exception.class)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public QNLGenericError handleException(Exception e) {
+		
+		QNLGenericError genericError = new QNLGenericError();
+		genericError.setCode(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
 		genericError.setErrorMessage(e.getMessage());
 		
 		return genericError;
--- src/main/java/es/tml/qnl/services/catalog/impl/CatalogServiceImpl.java
+++ src/main/java/es/tml/qnl/services/catalog/impl/CatalogServiceImpl.java
@@ -1,11 +1,9 @@
 package es.tml.qnl.services.catalog.impl;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import es.tml.qnl.beans.catalog.LoadDataRequest;
 import es.tml.qnl.beans.catalog.LoadDataResponse;
-import es.tml.qnl.exceptions.QNLException;
 import es.tml.qnl.services.catalog.CatalogService;
 
 @Service
@@ -15,10 +13,6 @@ public class CatalogServiceImpl implements CatalogService {
 	public LoadDataResponse loadData(LoadDataRequest request) {
 		
 		LoadDataResponse response = new LoadDataResponse();
-		
-		if (response.getMessage() == null) {
-			throw new QNLException(HttpStatus.INTERNAL_SERVER_ERROR, "Generic error from hell");
-		}
 		
 		return response;
 	}
